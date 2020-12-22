4cc695ec1f00dda5779ec46e376e95db03c85ed7
Pedro Santos <peterpedrods@gmail.com>
2020-12-21 21:43:40 -0300

    Search, verify end of gifs

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,10 +4,31 @@
       <img src="../assets/logo.png" alt="Home Logo" loading="lazy" />
     </div>
     <div class="search">
-      <input type="text" class="input" placeholder="Search" />
-      <button class="btn">
+      <input
+        type="text"
+        class="input"
+        v-model="searchText"
+        placeholder="Search"
+      />
+      <button class="btn" @click="onSearch(searchText)">
         <i class="fas fa-search"></i>
       </button>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      searchText: ""
+    };
+  },
+  methods: {
+    onSearch(text: string) {
+      this.$emit("search", text);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar @search="onSearch" />
     <div id="GridImages" class="container" v-if="imgs.length">
       <Gif
         v-for="(img, index) in imgs"
@@ -36,24 +36,48 @@ export default class Home extends Vue {
     ? parseInt(this.$route.params.page, 10)
     : 1;
   private isLoading = false;
+  private search = "";
+  private isAppend = true;
+  private limit = 25;
+  private max = this.limit;
+  private hasMore = true;
+  protected apikey = process.env.VUE_APP_GIHPY_API_KEY;
+  protected uri = "//api.giphy.com/v1/gifs";
+
+  public onSearch(value: string) {
+    this.page = 1;
+    this.max = this.limit;
+    this.isAppend = false;
+    this.search = value;
+    this.getGifs();
+  }
 
   public async getGifs() {
-    const apikey = process.env.VUE_APP_GIHPY_API_KEY;
-    const limit = 25;
-    const offset = (this.page - 1) * limit;
+    const offset = (this.page - 1) * this.limit;
+    const type = this.search.length ? "search" : "trending";
+    let uri = `${this.uri}/${type}?api_key=${this.apikey}&limit=${this.limit}&offset=${offset}&rating=g`;
+    if (this.search.length) {
+      uri += `&q=${this.search}`;
+    }
 
     this.isLoading = true;
 
     await axios
-      .get(
-        `//api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=${limit}&offset=${offset}&rating=g`
-      )
+      .get(uri)
       .then((value: AxiosResponse<Response>) => {
-        this.appendImages(value.data.data);
+        const response = value.data;
+        if (this.isAppend) {
+          this.appendImages(response.data);
+        } else {
+          this.imgs = response.data;
+        }
+        this.max = response.pagination.total_count;
+        this.hasMore = this.max > offset;
       })
       .catch(error => console.log(error));
 
     this.isLoading = false;
+    this.isAppend = true;
   }
 
   private appendImages(data: []) {
@@ -65,7 +89,7 @@ export default class Home extends Vue {
       const bottomOfWindow =
         document.documentElement.scrollTop + window.innerHeight ===
         document.documentElement.offsetHeight;
-      if (bottomOfWindow) {
+      if (bottomOfWindow && this.hasMore) {
         this.page += 1;
         this.getGifs();
       }
