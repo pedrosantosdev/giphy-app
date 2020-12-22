8bc378fd650998d039b4477fb3cb34aee0842789
Pedro Santos <peterpedrods@gmail.com>
2020-12-22 19:36:08 -0300

    262b25f

---
+++ precache-manifest.49cd6ee5c6b618fd69911078ac0eaae9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8ff33ca6a6a6b1c45e37819c73675c",
+    "url": "/giphy-app/index.html"
+  },
+  {
+    "revision": "65176cb7cb56f55bdea814e6afb2cecb",
+    "url": "/giphy-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/giphy-app/robots.txt"
+  },
+  {
+    "revision": "23ffc36803d6a10d5ac1",
+    "url": "/giphy-app/static/css/app.ebafc33d.css"
+  },
+  {
+    "revision": "8adbf433021d601ce9e39e069b1bdda3",
+    "url": "/giphy-app/static/img/loading.8adbf433.svg"
+  },
+  {
+    "revision": "23ffc36803d6a10d5ac1",
+    "url": "/giphy-app/static/js/app.9bad922a.js"
+  },
+  {
+    "revision": "d60aff2f0d1469d1a559",
+    "url": "/giphy-app/static/js/chunk-2d0e95df.4eb84f52.js"
+  },
+  {
+    "revision": "aee8aabfbd367e14b47f",
+    "url": "/giphy-app/static/js/chunk-vendors.1faf34d2.js"
+  }
+]);
