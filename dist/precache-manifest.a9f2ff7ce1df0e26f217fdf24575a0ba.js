2a9379195c563c1be18f7101f5f5f9775dba6c65
Pedro Santos <peterpedrods@gmail.com>
2020-12-22 18:34:30 -0300

    adding dist subtree

---
+++ dist/precache-manifest.a9f2ff7ce1df0e26f217fdf24575a0ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b28c7cb1d88516c2236c306b87b618",
+    "url": "/giphy-app/index.html"
+  },
+  {
+    "revision": "65176cb7cb56f55bdea814e6afb2cecb",
+    "url": "/giphy-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/giphy-app/robots.txt"
+  },
+  {
+    "revision": "fed05476815362cc7417",
+    "url": "/giphy-app/static/css/app.ebafc33d.css"
+  },
+  {
+    "revision": "8adbf433021d601ce9e39e069b1bdda3",
+    "url": "/giphy-app/static/img/loading.8adbf433.svg"
+  },
+  {
+    "revision": "fed05476815362cc7417",
+    "url": "/giphy-app/static/js/app.82f330fe.js"
+  },
+  {
+    "revision": "d60aff2f0d1469d1a559",
+    "url": "/giphy-app/static/js/chunk-2d0e95df.4eb84f52.js"
+  },
+  {
+    "revision": "aee8aabfbd367e14b47f",
+    "url": "/giphy-app/static/js/chunk-vendors.1faf34d2.js"
+  }
+]);
