0b29ae3fd3c8c9abb620142baa3daaf06da4e88a
Pedro Santos <peterpedrods@gmail.com>
2020-12-22 20:43:14 -0300

    Pwa configs, generator icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/giphy-app/" : "/",
   assetsDir: "static",
-  lintOnSave: process.env.NODE_ENV !== "production"
+  lintOnSave: process.env.NODE_ENV !== "production",
+  name: "Giphy Search App",
+  themeColor: "#176b6b",
+  msTileColor: "#222222",
+  appleMobileWebAppCapable: "yes",
+  appleMobileWebAppStatusBarStyle: "black"
 };
