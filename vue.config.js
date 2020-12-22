1794ad6ed46c2ee0055c3d9cee5ef6201df7c4d0
Pedro Santos <peterpedrods@gmail.com>
2020-12-22 20:50:00 -0300

    vue.config.js correction

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,11 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/giphy-app/" : "/",
   assetsDir: "static",
   lintOnSave: process.env.NODE_ENV !== "production",
-  name: "Giphy Search App",
-  themeColor: "#176b6b",
-  msTileColor: "#222222",
-  appleMobileWebAppCapable: "yes",
-  appleMobileWebAppStatusBarStyle: "black"
+  pwa: {
+    name: "Giphy Search App",
+    themeColor: "#176b6b",
+    msTileColor: "#222222",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+  }
 };
